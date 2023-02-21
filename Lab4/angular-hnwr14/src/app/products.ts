60d5b956928f6211d3532114bd28651c0ba47512
Temirkhan Seken <98910038+Papichulo118@users.noreply.github.com>
2023-02-21 20:29:58 +0600

    --lab4

---
+++ Lab4/angular-hnwr14/src/app/products.ts
@@ -0,0 +1,128 @@
+export interface Product {
+  id: number;
+  name: string;
+  picture: string;
+  price: number;
+  description: string;
+  rating: number;
+  reference: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XR',
+    picture:
+      'https://cdn1.it4profit.com/brd2/q:68/g:sm/plain/s3://app/public/models/MRY92RM-A/large/j/190716160018581911.jpg',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    rating: 5,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-xr-64gb-slim-box-chernyi-100691239/?c=750000000#!/item',
+  },
+  {
+    id: 2,
+    name: 'Phone 12 Mini',
+    picture:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h44/h9c/17090025816094/apple-iphone-12-mini-128gb-sinij-100657856-1-Container.png',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 6,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-12-mini-64gb-chernyi-100657220/?c=750000000#!/item',
+  },
+  {
+    id: 3,
+    name: 'Phone 11 Pro Max',
+    picture:
+      'https://onepoint.kz/uploads/products/590/apple-iphone-11-pro-max-512gb-midnight-green.jpeg',
+    price: 299,
+    description: 'Greater than ever',
+    rating: 7,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-11-pro-64gb-seryi-1005129/?c=750000000#!/item',
+  },
+  {
+    id: 4,
+    name: 'Phone 12 Pro Max',
+    picture:
+      'https://www.technodom.kz/_next/image?url=https%3A%2F%2Fapi.technodom.kz%2Ff3%2Fapi%2Fv1%2Fimages%2F800%2F800%2F228637_1.jpg&w=3840&q=85',
+    price: 399,
+    description: 'Number one from numbers one',
+    rating: 10,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-12-pro-max-128gb-sinii-100658112/?c=750000000#!/item',
+  },
+  {
+    id: 5,
+    name: 'Phone 13 Pro Max',
+    picture:
+      'https://www.technodom.kz/_next/image?url=https%3A%2F%2Fapi.technodom.kz%2Ff3%2Fapi%2Fv1%2Fimages%2F800%2F800%2F252998_1.jpg&w=3840&q=85',
+    price: 499,
+    description: 'Ideal.',
+    rating: 10,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-13-pro-max-128gb-zelenyi-104079084/?c=750000000#!/item',
+  },
+  {
+    id: 6,
+    name: 'Phone 11 Pro',
+    picture:
+      'https://onepoint.kz/uploads/products/476/apple-iphone-11-pro-64gb-silver.jpeg',
+    price: 299,
+    description: 'Just style.',
+    rating: 6.5,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-11-pro-64gb-seryi-1005129/?c=750000000#!/item',
+  },
+  {
+    id: 7,
+    name: 'Phone 12 Pro',
+    picture:
+      'https://www.technodom.kz/_next/image?url=https%3A%2F%2Fapi.technodom.kz%2Ff3%2Fapi%2Fv1%2Fimages%2F800%2F800%2F228626_1.jpg&w=3840&q=85',
+    price: 399,
+    description: 'Just do it.',
+    rating: 10,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-12-pro-128gb-seryi-100657107/?c=750000000#!/item',
+  },
+  {
+    id: 8,
+    name: 'Phone 13 Pro',
+    picture:
+      'https://activ.kz/shop/media/products/iphone_13_pro123456_oXbD4y9.jpg',
+    price: 499,
+    description: 'More than you can imagine.',
+    rating: 10,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-13-pro-256gb-zelenyi-104079046/?c=750000000#!/item',
+  },
+  {
+    id: 9,
+    name: 'Phone 14 Pro Max',
+    picture:
+      'https://www.technodom.kz/_next/image?url=https%3A%2F%2Fapi.technodom.kz%2Ff3%2Fapi%2Fv1%2Fimages%2F800%2F800%2F265322_1v.jpg&w=3840&q=85',
+    price: 599,
+    description: '...',
+    rating: 10,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+  },
+  {
+    id: 10,
+    name: 'Phone 14 Pro',
+    picture:
+      'https://www.technodom.kz/_next/image?url=https%3A%2F%2Fapi.technodom.kz%2Ff3%2Fapi%2Fv1%2Fimages%2F800%2F800%2F265301_1.jpg&w=3840&q=85',
+    price: 499,
+    description: 'Easy to take pictures',
+    rating: 10,
+    reference:
+      'https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/?c=750000000#!/item',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
